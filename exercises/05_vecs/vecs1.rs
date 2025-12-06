bcaca6e15ce4363e063513b4a4864047ba49f53f
Rutvik Patel <rutvik.p4t3l@gmail.com>
2025-12-05 16:37:00 -0800

    chore: solve exercise [exercises/05_vecs/vecs1.rs]

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -5,11 +5,16 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // Use the vector macro.
     // let v = ???;
 
+    let v = vec![10, 20, 30, 40];
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    let (a, v) = array_and_vec();
+    assert_eq!(a, *v);
+    println!("Success!");
 }
 
 #[cfg(test)]
